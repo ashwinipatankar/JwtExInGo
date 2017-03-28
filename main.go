b307dcf86bae7ba9c432faaa66804f43a561fe21
Ashwini Patankar <ashwinispatankar@gmail.com>
2017-03-28 09:07:04 +0530

    Not running properly, need to fix few things

--- main.go
+++ main.go
@@ -1,7 +1,12 @@
 package main
 
 import (
+	"bytes"
+	cryptorand "crypto/rand"
+	"crypto/rsa"
+	"crypto/x509"
 	"encoding/json"
+	"encoding/pem"
 	"fmt"
 	"log"
 	"net/http"
@@ -26,6 +31,59 @@ type Address struct {
 }
 
 var people []Person
+
+var signingKey, verificationKey []byte
+
+func intiKeys() {
+	var (
+		err         error
+		privKey     *rsa.PrivateKey
+		pubKey      *rsa.PublicKey
+		pubKeyBytes []byte
+	)
+
+	privKey, err = rsa.GenerateKey(cryptorand.Reader, 2048)
+	if err != nil {
+		log.Fatal("Error generating private key")
+	}
+	pubKey = &privKey.PublicKey
+
+	//Create signingkey from priv key
+	//Prepare PEM block
+	var privPEMBlock = &pem.Block{
+		Type:  "RSA PRIVATE KEY",
+		Bytes: x509.MarshalPKCS1PrivateKey(privKey), //Marshal means searlize
+	}
+
+	//serialize pem
+	privKeyPEMBuffer := new(bytes.Buffer)
+	pem.Encode(privKeyPEMBuffer, privPEMBlock)
+
+	//done
+	signingKey = privKeyPEMBuffer.Bytes()
+
+	fmt.Println(string(signingKey))
+
+	//create verificationKey from pubKey, Also in PEM-format
+	pubKeyBytes, err = x509.MarshalPKIXPublicKey(pubKey)
+	if err != nil {
+		log.Fatal("Error marshalling public key")
+	}
+
+	var pubPEMBlock = &pem.Block{
+		Type:  "RSA PUBLIC KEY",
+		Bytes: pubKeyBytes,
+	}
+
+	pubKeyPEMBuffer := new(bytes.Buffer)
+	pem.Encode(pubKeyPEMBuffer, pubPEMBlock)
+
+	//done
+	verificationKey = pubKeyPEMBuffer.Bytes()
+
+	fmt.Println(string(verificationKey))
+
+}
 
 func GetPersonEndPoint(writer http.ResponseWriter, request *http.Request) {
 	params := mux.Vars(request)
@@ -86,6 +144,14 @@ func LoginHandler(w http.ResponseWriter, r *http.Request) {
 		fmt.Println("Error logging in because of username/password")
 		return
 	}
+
+	//Create a rsa 256 signer
+	//TODO: Add support to other ways by configuration file
+	//signer := jwt.New(jwt.SigningMethodRS256)
+
+	//set claims
+	//things are broken by this, time to change the library
+
 }
 func startServer() {
 	router := mux.NewRouter()
