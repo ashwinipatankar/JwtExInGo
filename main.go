4de9661e566521187975d1d45822fc0bec46558f
Ashwini Patankar <ashwinispatankar@gmail.com>
2017-04-01 17:32:41 +0530

    Added data elements, will modify the key generation module later

--- main.go
+++ main.go
@@ -76,6 +76,20 @@ type Token struct {
 	Token string `json:"token"`
 }
 
+type Person struct {
+	ID        string   `json:"id, omitempty"`
+	Firstname string   `json:"firstname , omitempty"`
+	Lastname  string   `json:"lastname, omitempty"`
+	Address   *Address `json:"address, omitempty"`
+}
+
+type Address struct {
+	City  string `json:"city, omitempty"`
+	State string `json:"state, omitempty"`
+}
+
+var people []Person
+
 //App claims provide custom claim for JWt
 type AppClaims struct {
 	UserName string `json:"username"`
@@ -107,8 +121,15 @@ func StartServer() {
 	http.ListenAndServe(":8000", nil)
 
 }
+
+func initData() {
+	people = append(people, Person{ID: "1", Firstname: "Ashwini", Lastname: "Patankar", Address: &Address{City: "Bangalore", State: "India"}})
+	people = append(people, Person{ID: "2", Firstname: "Manish", Lastname: "Patankar", Address: &Address{City: "San Fransico", State: "California"}})
+	people = append(people, Person{ID: "3", Firstname: "Hun", Lastname: "Patankar", Address: &Address{City: "Munich", State: "Germany"}})
+}
 func main() {
 	initKeys()
+	initData()
 	StartServer()
 }
 
