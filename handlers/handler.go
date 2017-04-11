0834036ca6ab302f25b422d63f7e61703032f0f8
Ashwini Patankar <ashwinispatankar@gmail.com>
2017-04-11 19:54:34 +0530

    Moved the login.html to html directory

--- handlers/handler.go
+++ handlers/handler.go
@@ -3,5 +3,5 @@ package handler
 import "net/http"
 
 var GetLoginPageHandler = http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-	http.ServeFile(w, r, "login.html")
+	http.ServeFile(w, r, "html/login.html")
 })
